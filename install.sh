f7ffe16a458f490c29b09fe63f36e1476de2bd81
nikos <nikosglikis@gmail.com>
2021-05-03 15:08:27 +0300

    - Version is now 1.3-SNAPSHOT

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+#export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
 #mvn install -DperformRelease=true -DcreateChecksum=true
 mvn install -DcreateChecksum=true -DperformRelease=true -Dgpg.passphrase=password
+#mvn deploy -DperformRelease=true -DcreateChecksum=true -Dgpg.passphrase=password
